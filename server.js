3a52005f05c2f44cb01964e509a3c88480c126f0
YO9plusplus <6630275021@student.chula.ac.th>
2025-11-05 16:20:41 +0700

    add mongo connection

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config({path:'./config/config.env'});
+
+connectDB();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
+});
